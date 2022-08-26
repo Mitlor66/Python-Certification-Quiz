90a9a1eb7a3d0e9029371e63e5c64849c25bc53a
timur <timur.yarol@gmail.com>
2022-08-26 22:35:21 +0100

    V0.1.1
    README.md for more information

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Python-Certification-Quiz
-A graphical tool to automatically provide questions to students that prepare for their Python Certification and give them the score at the end of the Quiz.
+A graphical tool to automatically provide questions to students that prepare for their Python Certification and 
+give them the score at the end of the Quiz.
 
 ## Version 0.1.0
 The program is able to load a json file in Python, read all the questions from it, select a random one and display
 it in the terminal. The user can then enter their answer. A check is made to ensure the answer entered by the user is 
 valid. If the input is valid, then the user's answer is compared with the correct answer. If the user is wrong, the
 correct answer is displayed.
+
+## Version 0.1.1
+Add of a GUI using Tkinter with a single question being displayed at the top as well as 2 or 4 buttons 
+(depending on how many possible answers).
--- quiz.py
+++ quiz.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import random
+import tkinter as tk
+import tkinter.font as font
+from tkinter import ttk
 
 
 def read_file(filename):
     return pd.read_json(filename)
 
 
-def generate_question():
-    r = random.choice(possible_questions_id)
+def generate_question(questions):
+    number_of_questions = len(questions)
+    possible_questions_id = [x for x in range(number_of_questions)]
+    r = 2  # random.choice(possible_questions_id)
     possible_questions_id.remove(r)
     print("question ID:", r+1)
     with open(f"questions/Q{r+1}.txt", 'r') as f:
@@ -24,18 +29,97 @@ def generate_question():
     a = ""
     possible_answers = choices.keys()
     print(possible_answers)
-    while a not in possible_answers:
+    """while a not in possible_answers:
         a = input("What is your answer? ")
         if a not in possible_answers:
             print("Wrong input, please enter one of the following possible answer:", possible_answers)
         elif a == answer:
             print("well done")
         else:
-            print("wrong, sorry, the answer was", answer)
+            print("wrong, sorry, the answer was", answer)"""
+    return question, choices, answer
 
 
-data = read_file("all questions.json")
-questions = data["questions"]
-number_of_questions = len(questions)
-possible_questions_id = [x for x in range(number_of_questions)]
-generate_question()
+def select(choice, answer):
+    print(choice, answer)
+    if choice == answer:
+        print("well done")
+    else:
+        print("wrong, sorry, the answer was", answer)
+
+
+def create_quiz():
+    data = read_file("all questions.json")
+    questions = data["questions"]
+
+    question, choices, answer = generate_question(questions)
+
+    root = tk.Tk()
+    root.title('Python Certification Quiz')
+
+    window_width = 1000
+    window_height = 600
+
+    s = ttk.Style()
+    s.configure('my.TButton', font=('Helvetica', 18), width=37)
+
+    #text_font = font.Font(size=30, family='Helvetica')
+
+    # get the screen dimension
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # find the center point
+    center_x = int(screen_width/2 - window_width / 2)
+    center_y = int(screen_height/2 - window_height / 2)
+
+    # set the position of the window to the center of the screen
+    root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+    root.resizable(False, False)
+
+    question_frame = tk.Frame(root)
+    question_frame.pack(expand=True, fill='x')
+
+    buttons_frame = tk.Frame(root)
+    buttons_frame.pack(fill='x')
+
+    label_question = tk.Label(
+        question_frame,
+        text=question,
+        font=('Helvetica', 18),
+        bd=1, relief='sunken',
+        justify='left'
+    )
+
+    label_question.pack(
+        ipadx=5,
+        ipady=5,
+        expand=True
+    )
+
+    number_of_choices = len(choices.items())
+    x = 0
+    y = 0
+    for k, v in choices.items():
+        if number_of_choices == 2:
+            button = ttk.Button(master=buttons_frame,
+                                text=f'{k}: {v}',
+                                style='my.TButton',
+                                command=lambda t=k: select(t, answer))
+            button.grid(row=x, column=y, ipadx=10, ipady=10)
+            y += 1
+        elif number_of_choices == 4:
+            button = ttk.Button(master=buttons_frame,
+                                text=f'{k}: {v}',
+                                style='my.TButton',
+                                command=lambda t=k: select(t, answer))
+            button.grid(row=x, column=y, ipadx=10, ipady=10)
+            y += 1
+            if y == 2:
+                y = 0
+                x += 1
+
+    root.mainloop()
+
+
+create_quiz()
